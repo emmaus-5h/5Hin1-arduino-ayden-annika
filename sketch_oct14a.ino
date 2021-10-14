9c7afcc4fb47fe488b02f1f1cebe6b3eddb72f43
AIdan19 <57942160+AIdan19@users.noreply.github.com>
2021-10-14 13:20:24 +0200

    Add files via upload
    v2

--- sketch_oct14a.ino
+++ sketch_oct14a.ino
@@ -12,9 +12,15 @@ pinMode(6, INPUT);
 void loop() {
   // put your main code here, to run repeatedly:
 if (toestand == buttonUit){
-digitalWrite(6, LOW);
-digitalWrite(5, LOW);
-
+  if(digitalRead(6) == HIGH){
+    digitalWrite(5, HIGH);
+    toestand = buttonAan;
+    }
   }
-  
+  if (toestand == buttonAan){
+  if(digitalRead(6) == LOW){
+    digitalWrite(5, LOW);
+    toestand = buttonUit;
+    }
+  }
 }
